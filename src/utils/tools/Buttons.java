3fa03baa350ca86d25057509356d8c5cfe130182
Gopal Rao <gopalkrao3@gmail.com>
2013-11-08 12:16:12 -0800

    Added new attribute to disable to fix #2

--- src/utils/tools/Buttons.java
+++ src/utils/tools/Buttons.java
@@ -1,6 +1,10 @@
+/**
+*  Author : Gopal Rao
+*/
 public class Buttons {
 	private String id;
 	private String label;
+	private boolean disabled;
 	public Buttons() {
 	}
 
@@ -18,7 +22,15 @@ public class Buttons {
 	
 	public String getLabel() {
 		return this.label;
-	]
+	}
+
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+
+	public boolean isDisabled() {
+		return this.disabled;
+	}
 
 }
 
