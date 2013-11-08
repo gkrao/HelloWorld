a50c1caca68116d0bcfb4a00ba3f8febea11bbcf
Gopal Rao <gopalkrao3@gmail.com>
2013-11-08 11:50:40 -0800

    test test

---
+++ src/utils/tools/Buttons.java
@@ -0,0 +1,24 @@
+public class Buttons {
+	private String id;
+	private String label;
+	public Buttons() {
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	]
+
+}
+
